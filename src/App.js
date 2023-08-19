1863c10d5ce904df481f1c830b19d67e5dba561f
SiamAnzir <siamanzir18@gmail.com>
2023-08-19 23:42:44 +0600

    added-react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddProject from "./components/AddProject";
+import ProjectListComponent from "./components/ProjectList";
+import useLocalStorage from "./hooks/useLocalStorage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [projectList , setProjectList] = useLocalStorage("projectList" , []);
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<AddProject projectList={projectList} setProjectList={setProjectList}></AddProject>} />
+                    <Route path="/projectList" element={<ProjectListComponent projectList={projectList}></ProjectListComponent>} />
+                    <Route path="*" element={ <p>404 NOT FOUND</p>} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+};
 
 export default App;
